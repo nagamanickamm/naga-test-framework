a2b886d5daef33335beb147e1ac808a8f46dbe40
nagamanickamm <50880478+nagamanickamm@users.noreply.github.com>
2023-03-26 17:53:14 +0100

    Delete install_dependencies.ps1

--- scripts/local/install_dependencies.ps1
+++
@@ -1,3 +0,0 @@
-$username = Read-Host 'Enter - Username'
-$pat = Read-Host 'Enter - PAT'
-pip install --upgrade --index-url https://${username}:${pat}@tfs.engineering.intelligentgaming.net/tfs/Neon-Collection/_packaging/Neon/pypi/simple -r neon\requirements.txt
